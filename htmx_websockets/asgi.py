9d1752492d24bfc54d24448871bf39e6538524a5
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-08 15:21:09 +0200

    Create connection with websocket

--- htmx_websockets/asgi.py
+++ htmx_websockets/asgi.py
@@ -3,6 +3,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+import core.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "htmx_websockets.settings")
 
@@ -11,5 +12,7 @@ application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": application,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(core.routing.websocket_urlpatterns))
+    ),
 })

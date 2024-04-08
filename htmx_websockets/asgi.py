94718c21ce07052436efca805e08002f31be1194
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-08 15:18:42 +0200

    Update asgi.py

--- htmx_websockets/asgi.py
+++ htmx_websockets/asgi.py
@@ -1,5 +1,7 @@
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "htmx_websockets.settings")

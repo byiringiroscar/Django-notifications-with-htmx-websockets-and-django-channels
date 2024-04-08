e8c37446edc401959e659e89197df4c78ec90921
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-08 14:51:46 +0200

    Update asgi.py

--- htmx_websockets/asgi.py
+++ htmx_websockets/asgi.py
@@ -1,6 +1,13 @@
 import os
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "htmx_websockets.settings")
 
 application = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http": application,
+    # Just HTTP for now. (We can add other protocols later.)
+})

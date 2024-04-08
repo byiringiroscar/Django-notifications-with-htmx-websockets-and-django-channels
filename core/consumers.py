110a83d46551f91eb1f02cafeb268b1b3890b19c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-08 16:16:11 +0200

    Create group

--- core/consumers.py
+++ core/consumers.py
@@ -5,6 +5,8 @@ from asgiref.sync import async_to_sync
 
 class NotificationConsumer(WebsocketConsumer):
     def connect(self):
+        self.GROUP_NAME = "user-notifications"
+        async_to_sync(self.channel_layer.group_add)(self.GROUP_NAME, self.channel_name)
         self.accept()
     
     def disconnect(self, close_code):

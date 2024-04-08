fcf68876b8a56150f1cf01d1305563ccbeecc218
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-08 17:03:06 +0200

    Update consumers.py

--- core/consumers.py
+++ core/consumers.py
@@ -6,12 +6,18 @@ from django.template.loader import get_template
 
 class NotificationConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope["user"] # get the user from the scope object of the connection instance
+        # send a message to the client that has connected, with no content at this time
+        if not self.user.is_authenticated: # if the user is not authenticated, close the connection
+            self.close()
+            return
         self.GROUP_NAME = "user-notifications"
         async_to_sync(self.channel_layer.group_add)(self.GROUP_NAME, self.channel_name)
         self.accept()
     
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(self.GROUP_NAME, self.channel_name)
+        if self.user.is_authenticated:
+            async_to_sync(self.channel_layer.group_discard)(self.GROUP_NAME, self.channel_name)
 
     
 
